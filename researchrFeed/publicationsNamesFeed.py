671b32bd2d056bcfdbe044d8631f286c8c605591
Michal Cab <xcabmi00@athena2.fit.vutbr.cz>
2013-06-11 09:35:30 +0200

    Add publications names downloader

---
+++ researchrFeed/publicationsNamesFeed.py
@@ -0,0 +1,8 @@
+from researchr import *
+
+r = ResearchrClass()
+
+print "Zahajeni stahovani jmen publikaci:"
+print "r.getPublicationsNames(self, \"testFile.txt\", \"\")"
+r.getPublicationsNames("testFile.txt", "")
+print "Stahovani bylo uspesne dokonceno"
